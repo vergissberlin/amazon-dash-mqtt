302f9e2dd3a1f83dbdfd4cc69f040f37c4a061c0
André Lademann <vergissberlin@googlemail.com>
2018-08-30 23:42:39 +0200

    Bring the notification to work

--- index.js
+++ index.js
@@ -7,45 +7,41 @@
  */
 
 const request = require('request')
-
+const DashButton = require('dash-button')
 require('dotenv').config()
-require('dash-button')
 
-function sendToSlack (s, theUsername, theIconUrl, theIconEmoji, theChannel) {
+function sendToSlack(s, theUsername, theIconUrl, theIconEmoji, theChannel) {
     let payload = {
         text: s
     }
-    if ( theUsername !== undefined ) {
+    if (theUsername !== undefined) {
         payload.username = theUsername
     }
-    if ( theIconUrl !== undefined ) {
+    if (theIconUrl !== undefined) {
         payload.icon_url = theIconUrl
     }
-    if ( theIconEmoji !== undefined ) {
+    if (theIconEmoji !== undefined) {
         payload.icon_emoji = theIconEmoji
     }
-    if ( theChannel !== undefined ) {
+    if (theChannel !== undefined) {
         payload.channel = theChannel
     }
     let theRequest = {
-        url:    process.env.SLACK_URL,
+        url: process.env.SLACK_URL,
         method: 'POST',
-        json:   payload
+        json: payload
     }
-    request(theRequest, function (error, response) {
-        if ( !error && (response.statusCode === 200) ) {
+    request(theRequest, function(error, response) {
+        if (!error && (response.statusCode === 200)) {
             console.log('sendToSlack: ' + s)
-        }
-        else {
+        } else {
             console.log('sendToSlack: error, code == ' + response.statusCode + ', ' + response.body + '.\n')
         }
     })
 }
 
-
 let button = new DashButton(process.env.DASH_BUTTON_DEPLOY)
 
-button.addListener(async() => {
-    sendToSlack('... is calling you', 'The Boss', 'http://beardstyle.net/wp-content/uploads/2016/06/hipster-mustaches-11.jpg', null, 'ci')
+button.addListener(async () => {
+    sendToSlack('... is calling you', 'The Boss', 'http://beardstyle.net/wp-content/uploads/2016/06/hipster-mustaches-11.jpg', null, '@ala')
 })
-
