ca5e2e51dd000189caf356bfe9f08a6ece123dd9
Andre Lademann <andre.lademann@netresearch.de>
2018-09-21 16:47:33 +0200

    Go live

---
+++ mqtt.js
@@ -0,0 +1,31 @@
+/**
+ * Send slack message
+ *
+ * @category
+ * @author   Andre Lademann <vergissberlin@googlemail.com>
+ * @license  MIT
+ */
+
+const mqtt = require('mqtt')
+var client  = mqtt.connect(
+{
+        host: 'mqtt://io.adafruit.com',
+        port: 1883,
+        username: 'vergissberlin',
+        password: 'ad755b7b9d994e4abe62bc0ace181023'
+    }
+)
+
+client.on('connect', function () {
+  client.subscribe('vergissberlin/feeds/dash', function (err) {
+    if (!err) {
+      client.publish('vergissberlin/feeds/dash', '50000')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
